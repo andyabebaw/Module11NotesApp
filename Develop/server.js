fd14ac62a648090d4736dba7ad08f457d7799af0
Andy Abebaw <andyabebaw1@gmail.com>
2023-02-01 17:58:07 -0800

    add load notes support

--- Develop/server.js
+++ Develop/server.js
@@ -20,6 +20,8 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
+});
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
